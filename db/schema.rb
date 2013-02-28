60da4b979b5b518f3f5077ae560e130da251e4f9
Sandra McMullen <smcmullen101@gmail.com>
2013-02-28 15:55:28 -0600

    calendar now has dates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130227183722) do
+ActiveRecord::Schema.define(:version => 20130228175125) do
+
+  create_table "calendar_entries", :force => true do |t|
+    t.time     "date"
+    t.integer  "slot"
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "poats", :force => true do |t|
     t.string   "name"
