12096370bf8983931fb477ffa1e31cc157dda0b4
Sandra McMullen <yamainu.no.tokoyami@gmail.com>
2013-04-28 18:18:45 -0500

    Randomly generating tanuki(gender,name,nature)

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,17 +1,41 @@
 class ProfileController < ApplicationController
   def index
   	@tanukis = Tanuki.all
+    @gentanukis = Array.new()
 
-  	if(rand(0..1))
-  		@gentanuki = Tanuki.new(:male => "true", :name_id => MaleName.first(:offset => rand(MaleName.count)).id, :nature_id => Nature.first(:offset => rand(Nature.count)).id)
-  	else
-  		@gentanuki = Tanuki.new(:male => "false", :name_id => FemaleName.first(:offset => rand(FemaleName.count)).id, :nature_id => Nature.first(:offset => rand(Nature.count)).id)
-  	end
+    3.times do
+      gentanuki = Tanuki.new()
+
+    	if([true, false].sample)
+    		gentanuki.male = "true"
+        gentanuki.name = Tanuki.get_random_male_name
+        gentanuki.nature = Tanuki.get_random_nature
+    	else
+        gentanuki.male = "false"
+        gentanuki.name = Tanuki.get_random_female_name
+        gentanuki.nature = Tanuki.get_random_nature
+    	end
+      @gentanukis << gentanuki
+    end
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @tanukis }
-      format.json { render json: @gentanuki }
+      format.json { render json: @gentanukis }
     end
   end
+
+  def adopt_tanuki(selected)
+    @tanuki = @gentanukis[selected]
+
+    respond_to do |format|
+      if @tanuki.save
+        format.html { redirect_to @tanuki, notice: 'Task was successfully created.' }
+        format.json { render json: @tanuki, status: :created, location: @tanuki }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @tanuki.errors, status: :unprocessable_entity }
+      end
+    end
+  end   
 end
--- app/models/tanuki.rb
+++ app/models/tanuki.rb
@@ -7,31 +7,67 @@ class Tanuki < ActiveRecord::Base
   validates :male, :presence => true  
   validates :nature, :presence => true 
 
-=begin
-	open(File.join(File.dirname(__FILE__), "f-names.txt")) do |fnames|
-	  fnames.read.each_line_with_index do |name, index|
-	    name, description = name.chomp.split(",", 2)
-	    description.chop!.slice!(0)
-	    newname = FemaleName.create!(:name => name)
-	    newname.id = index
-	  end
-	end  
+	MALE_NAMES = Array.new(1)
+	FEMALE_NAMES = Array.new(1)
+	NATURES = Array.new(1)
+
 
 	open(File.join(File.dirname(__FILE__), "m-names.txt")) do |mnames|
-	  mnames.read.each_line_with_index do |name, index|
+	  mnames.read.each_line do |name, index|
 	    name, description = name.chomp.split(",", 2)
 	    description.chop!.slice!(0)
-	    newname = MaleName.create!(:name => name)
-	    newname.id = index
+	    MALE_NAMES << [name, description]
 	  end
 	end
 
-	open(File.join(File.dirname(__FILE__), "natures.txt")) do |natures|
-	  natures.read.each_line_with_index do |nature, index|
-	    nature.chomp!
-	    newnature = Nature.create!(:description => nature)
-	    newnature.id = index
+	open(File.join(File.dirname(__FILE__), "f-names.txt")) do |fnames|
+	  fnames.read.each_line do |name, index|
+	    name, description = name.chomp.split(",", 2)
+	    description.chop!.slice!(0)
+	    FEMALE_NAMES << [name, description]
 	  end
 	end	
-=end
+
+	open(File.join(File.dirname(__FILE__), "natures.txt")) do |natures|
+	  natures.read.each_line do |nature, index|
+	    nature.chomp!
+	    NATURES << nature
+	  end
+	end	
+
+	def self.get_random_male_name
+		rand(MALE_NAMES.count)
+	end
+
+	def self.get_random_female_name
+		rand(FEMALE_NAMES.count)
+	end	
+
+	def self.get_random_nature
+		rand(NATURES.count)
+	end	
+
+	def get_name
+		if(self.male == true)
+			MALE_NAMES[self.name][0]
+		else
+			FEMALE_NAMES[self.name][0]
+		end
+	end
+
+	def get_name_meaning
+		if(self.male == true)
+			MALE_NAMES[self.name][1]
+		else
+			FEMALE_NAMES[self.name][1]
+		end
+	end	
+
+	def is_male
+		self.male
+	end		
+
+	def get_nature
+		NATURES[self.nature]
+	end		
 end
