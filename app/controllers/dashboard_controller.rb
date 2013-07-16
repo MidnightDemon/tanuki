040fdcf43d8a86c35692ca7c83362877b08c738f
Sandra McMullen <smcmullen101@gmail.com>
2013-07-16 15:16:41 +0000

    Small change for postgres to handle nil

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,6 +8,7 @@ class DashboardController < ApplicationController
   
     @entries = current_user.calendar_entries.last(5).reverse
     @posts = Post.last(5).reverse
+    @user = current_user
 
     @tasks_done = current_user.tasks.where(:completed => true);
     @tasks_pending = current_user.tasks.where(:completed => false).last(5).reverse;
@@ -17,7 +18,7 @@ class DashboardController < ApplicationController
     @users = User.all
     @users_search_results = User.search(params[:search])
 
-    @tanukis = current_user.tanukis
+    @tanukis = current_user.tanukis 
 
     respond_to do |format|
       format.html { render :layout => 'application' }
@@ -27,11 +28,31 @@ class DashboardController < ApplicationController
     end
   end  
 
+  def update
+    @user = current_user
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end  
+
   def show_date
     @date_entries = current_user.entries_for_date(params[:year], params[:month], params[:day])
     @time = Time.new(params[:year], params[:month], params[:day])
     @calories = 0
     
+    @date_entries.each do |entry|
+      if !entry.calories
+        entry.calories = 0
+      end
+    end
+
     # render :partial => "show_date"
     # render "edit"
 
